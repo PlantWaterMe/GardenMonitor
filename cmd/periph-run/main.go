ad5de2e09e77a6cb424f6c499e7f201654725cef
João Bolas <jbolasoares21@gmail.com>
2023-08-06 14:25:50 +0100

    test

--- cmd/periph-run/main.go
+++ cmd/periph-run/main.go
@@ -18,6 +18,20 @@ func main() {
 		log.Println("H3")
 	}
 
+	if bcm283x.GPIO18.Read() {
+		log.Println("GPIO18 is high")
+	} else {
+		log.Println("GPIO18 is low")
+	}
+
 	bcm283x.GPIO18.Out(true)
+
+	if bcm283x.GPIO1.Read() {
+		log.Println("GPIO1 is high")
+	} else {
+		log.Println("GPIO1 is low")
+	}
+
+	bcm283x.GPIO1.Out(true)
 
 }
