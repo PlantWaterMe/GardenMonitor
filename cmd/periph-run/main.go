c87f019a8e2fc3188adf870a6e620ca715cbb21f
João Bolas <jbolasoares21@gmail.com>
2023-08-06 13:46:51 +0100

    basic test for periph

--- cmd/periph-run/main.go
+++ cmd/periph-run/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"time"
-
 	"periph.io/x/conn/v3/gpio"
 	"periph.io/x/host/v3"
 	"periph.io/x/host/v3/allwinner"
@@ -10,9 +8,6 @@ import (
 
 func main() {
 	host.Init()
-	t := time.NewTicker(500 * time.Millisecond)
-	for l := gpio.Low; ; l = !l {
-		allwinner.PD14.Out(l)
-		<-t.C
-	}
+
+	allwinner.PD14.Out(gpio.High)
 }
