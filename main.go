76dfcc40c3ca485606f1ebd7dad295b9eb9e80a0
João Bolas <jbolasoares21@gmail.com>
2023-08-06 23:12:18 +0100

    updated read settings

--- main.go
+++ main.go
@@ -1,23 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"periph.io/x/conn/v3/driver/driverreg"
-	"periph.io/x/conn/v3/gpio"
 	"periph.io/x/conn/v3/gpio/gpioreg"
 	"periph.io/x/host/v3"
 	"periph.io/x/host/v3/allwinner"
 )
 
 func main() {
-	_, err := host.Init()
+	// Make sure periph is initialized.
+	state, err := host.Init()
 	if err != nil {
-		panic(err)
+		log.Fatalf("failed to initialize periph: %v", err)
 	}
 
-	if allwinner.IsH3() {
-		log.Println("H3")
+	// Prints the loaded driver.
+	fmt.Printf("Using drivers:\n")
+	for _, driver := range state.Loaded {
+		fmt.Printf("- %s\n", driver)
+	}
+
+	// Prints the driver that were skipped as irrelevant on the platform.
+	fmt.Printf("Drivers skipped:\n")
+	for _, failure := range state.Skipped {
+		fmt.Printf("- %s: %s\n", failure.D, failure.Err)
+	}
+
+	// Having drivers failing to load may not require process termination. It
+	// is possible to continue to run in partial failure mode.
+	fmt.Printf("Drivers failed to load:\n")
+	for _, failure := range state.Failed {
+		fmt.Printf("- %s: %v\n", failure.D, failure.Err)
 	}
 
 	err = allwinner.PA16.Out(true)
@@ -41,14 +57,15 @@ func main() {
 	// set as input or output.
 	log.Printf("%s is %s\n", p, p.Read())
 
-	err = inp.In(gpio.PullDown, gpio.RisingEdge)
-	if err != nil {
-		log.Println(err)
-	}
+	/*
+		err = inp.In(gpio.PullDown, gpio.RisingEdge)
+		if err != nil {
+			log.Println(err)
+		}
 
-	log.Printf("PA16 is %s", allwinner.PA16.Read())
-	log.Printf("GPIO17 is %s", p.Read())
-
+		log.Printf("PA16 is %s", allwinner.PA16.Read())
+		log.Printf("GPIO17 is %s", p.Read())
+	*/
 	for {
 		if inp.Read() {
 			log.Println("PA1 is high")
